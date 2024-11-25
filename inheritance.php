4e94059d6581becdda60bac121b7d32ecd2f32c3
DWL22 <candra.icy@gmail.com>
2024-11-25 16:06:41 +0800

    inheritance selesai lanjut ke overriding

---
+++ inheritance.php
@@ -0,0 +1,71 @@
+<?php 
+
+//jualan produk
+//kokik
+//game
+
+class Produk {
+    public $judul, 
+            $penulis,
+            $penerbit ,
+            $harga,
+            $jmlHalaman,
+            $waktuMain;
+            
+ 
+            public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jumlahHalaman = 0, $waktuMain= 0 ) {
+                $this->judul = $judul;
+                $this->penulis = $penulis;
+                $this->penerbit = $penerbit;
+                $this->harga = $harga;
+                $this->jmlHalaman = $jumlahHalaman;
+                $this->waktuMain = $waktuMain;
+            
+            }
+            public function getLabel() {
+                return "$this->penulis, $this->penerbit";
+            }
+
+            public function getInfoProduk() {
+                $str = " {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+            
+                return $str;
+            }
+}
+
+class Komik extends Produk {
+    public function getInfoProduk()
+    {
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";  
+        return $str;
+    }
+}
+
+class Game extends Produk {
+    public function getInfoProduk()
+    {
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";  
+        return $str;  
+    }
+}
+
+class CetakInfoProduk {
+    public function cetak(Produk $produk) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+
+$produk1 = new Komik("Narutor","kishimoto","sony jump",30000, 100, 0);
+$produk2 = new Game("uncharted","neil des","sony",250000, 0, 50);
+
+
+// Komik :Naruto | kishimoto, sony jump
+// Game :neil des, sony
+// Narutor | kishimoto, sony jump (Rp. 30000)
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
