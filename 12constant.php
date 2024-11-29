3fcf215f7a43fff15077187fa7d742548b538d1e
DWL22 <candra.icy@gmail.com>
2024-11-29 11:05:28 +0800

    video 12 tentang constant, Done!

---
+++ 12constant.php
@@ -0,0 +1,21 @@
+<?php 
+
+// define('NAMA', 'candra');
+// echo NAMA;
+// echo "<br>";
+// const UMUR = 30;
+// echo UMUR;
+
+
+// class Coba {
+//    const UMUR = 30;
+
+// }
+// echo Coba::UMUR;
+
+// echo __FUNCTION__;
+function coba () {
+    return __FUNCTION__;
+}
+echo coba();
+?>
